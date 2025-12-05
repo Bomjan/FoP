fc764441a12fabc52adbf3dbd62d4c588132d85f
Bomjan <sundrabomjan@gmail.com>
2025-12-05 21:01:34 +0600

    set 1 completed

--- 04-Exams/2024_practical_test/set_1/q1.js
+++ 04-Exams/2024_practical_test/set_1/q1.js
@@ -12,11 +12,10 @@ let students = [];
 function registerStudent(name, age, course) {
   idCounter++;
   students.push(new Students(idCounter, name, age, course));
-  console.log(`${name} with id ${idCounter} was regidtered for ${course}`);
+  console.log(
+    `${name} ${age} with id ${idCounter} was regidtered for ${course}`
+  );
 }
-
-registerStudent("Bomjan", 600, "SOC");
-registerStudent("Thinley", 1000, "SOC 8");
 
 function removeStudent(id) {
   students.forEach((std, ind) => {
@@ -57,9 +56,40 @@ const listStudents = () => {
 };
 
 const displayMenu = () => {
-  console.log(
-    "1. Add Student \n2. Remove Student \n3. Display All Students \n4. Exit"
-  );
+  return "1. Add Student \n2. Remove Student \n3. Display All Students \n4. Exit";
 };
 
-displayMenu();
+let exited = false;
+function showMenu() {
+  let res = prompt(displayMenu());
+
+  switch (res) {
+    case "1":
+      let name = prompt("Enter student name: ");
+      let age = prompt("Enter student age: ");
+      let course = prompt("Enter student course: ");
+
+      if (age > 0) {
+        registerStudent(name, parseInt(age), course);
+      } else {
+        console.log("Age cannot be negative bro");
+      }
+      break;
+    case "2":
+      let id = parseInt(prompt("enter id"));
+      removeStudent(id);
+      break;
+
+    case "3":
+      listStudents();
+      break;
+
+    case "4":
+      exited = true;
+      break;
+  }
+}
+
+while (!exited) {
+  showMenu();
+}
